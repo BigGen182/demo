778e41d6e79c1628084e296ad0c0ae1dde4df866
Zhang <shanzhangd@isoftstone.com>
2019-07-10 22:37:24 +0800

    first commit

---
+++ src/components/child.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <h3>我是子组件！</h3>
+</template>
+<script>
+  export default {
+    mounted: function () {
+      this.$emit('getMessage', '我是来自子组件的消息')
+    }
+  }
+</script>
---
+++ src/components/father.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <child @getMessage="showMsg"></child>
+  </div>
+</template>
+
+<script>
+  import Child from '../components/child.vue'
+  export default {
+    components: {Child},
+    data(){
+      return{
+        title:''
+      }
+    },
+    methods:{
+      showMsg(title){
+        this.title=title;
+      }
+    }
+  }
+</script>
